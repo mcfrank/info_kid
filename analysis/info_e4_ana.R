2044f24dae182b4f79cc5799309af8f8698b6080
Michael C. Frank <mcfrank@stanford.edu>
2014-06-23 11:06:52 -0700

    revision including Experiment 4

--- analysis/info_e4_ana.R
+++ analysis/info_e4_ana.R
@@ -11,6 +11,8 @@ d3$expt <- "salience"
 d <- rbind(d1,d2,d3)
 d$expt <- factor(d$expt, levels=c("replication","disambiguation","salience"))
 d$age <- d$age_group
+d$age[d$age >= 5] <- 4.99 # rounding error in the anonymization process
+
 md <- melt(d, id.vars=c("SID","age","expt"),
            measure.vars=names(d)[grepl("ambig",names(d)) | 
                                    grepl("inference",names(d))],
@@ -31,13 +33,27 @@ ms <- ddply(mss, .(trial.type,expt,age_group),
             summarise,          
             cil = ci.low(correct),
             cih = ci.high(correct),
+            n = length(correct),
+            age = mean(age),
+            sd = sd(correct,na.rm=TRUE),
             correct = mean(correct))
 
-quartz()
+#quartz()
 qplot(age_group, correct, fill = trial.type,
       stat="identity",
       position=position_dodge(.9),
       ymin=correct-cil, ymax=correct+cih,
       data=ms, facets = .~expt, geom=c("bar","linerange"),
       xlab="Age Group (Years)",ylab="Proportion Inference Consistent") + 
-  geom_hline(yintercept=.5,lty=2) 
+  geom_hline(yintercept=.5, lty=2) 
+
+# l2 <- lmer(correct ~ age.group + type  + (type|subid) + (1 |item),
+#            data=d1,family="binomial")
+
+md$age_group <- factor(floor(md$age))
+
+l1 <- lmer(correct ~ age_group * expt + (1 | SID) + (1 | variable),
+             data=subset(md, trial.type=="inference"))
+l2 <- lmer(correct ~ age_group + expt + (1 | SID) + (1 | variable),
+             data=subset(md, trial.type=="inference"))
+anova(l2,l1)
